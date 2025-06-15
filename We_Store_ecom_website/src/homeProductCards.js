2fde34d71fca10ef5c67df94f2534c3c3992e03d
vishalvrma.404@gmail.com <vishalvrma.404@gmail.com>
2025-06-15 23:53:27 +0530

    Added functionality to the product card of quantity selection.

--- We_Store_ecom_website/src/homeProductCards.js
+++ We_Store_ecom_website/src/homeProductCards.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate");
 
@@ -11,6 +13,8 @@ export const showProductsContainer = (products) =>{
 
         const productClone = document.importNode(productTemplate.content, true);
 
+        productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+
         productClone.querySelector(".productName").textContent = name;
         productClone.querySelector(".category").textContent = category;
         productClone.querySelector(".productImage").src = image;
@@ -19,6 +23,11 @@ export const showProductsContainer = (products) =>{
         productClone.querySelector(".productStock").textContent = stock;
         productClone.querySelector(".productPrice").textContent = `₹${price}`;
         productClone.querySelector(".productActualPrice").textContent = `₹${price*4}`;
+
+
+        productClone.querySelector(".stockElement").addEventListener("click", (event) => {
+            homeQuantityToggle(event, id, stock);
+        });
 
         productContainer.append(productClone);
 
